74ee82a7dbdfbbffff3ebb31dbff26e1c2357474
John Everett <john.everett@shaw.ca>
2017-06-05 19:16:49 +0000

    Add one to many relationships between chef aand recipes

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -2,34 +2,36 @@ require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
   def setup
-    @recipe = Recipe.new(name: 'vegetable', description: 'great vegetable recipe')
+    @chef = Chef.create!(chefname: 'Jack', email: 'jack@example.com')
+    @recipe = @chef.recipes.build(name: "vegetable", description: "great vegetable recipe")
   end
   
   test "recipe should be valid"  do
     assert @recipe.valid?
   end
   
+  test "recipe without chef should be invalid" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
+  end
+  
   test "name should be present" do
     @recipe.name = ""
-    @recipe.save
     assert_not @recipe.valid?
   end
   
     test "description should be present" do
     @recipe.description = ""
-    @recipe.save
     assert_not @recipe.valid?
   end
   
   test "description should not be less than 5 characters" do
     @recipe.description = "a" * 4
-    @recipe.save
     assert_not @recipe.valid?    
   end
   
   test "description should not be more than 500 characters" do
     @recipe.description = "a" * 501
-    @recipe.save
     assert_not @recipe.valid?
 
   end
